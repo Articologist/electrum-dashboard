c76597c95b5fe0abbb9a4dca4a0cd07f240792c7
Daniele Servadei <servadei.d@gmail.com>
2021-08-29 13:53:13 +0200

    multi crypto dashboard support

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const express = require("express")
 const fetch = require("node-fetch")
+const fs = require("fs")
 const path = require("path")
 const app = express()
 
 app.set("views", path.join(__dirname, "/pages"))
 app.set("view engine", "ejs")
 
-const {
-  rpcUser,
-  rpcPassword,
-  rpcHost,
-  rpcPort
-} = process.env
+const config = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json"), "utf8"))
 
-const crypto = process.env.crypto.toUpperCase()
+const rpcRequest = async (body, crypto) => {
+  const {
+    rpcUser,
+    rpcPassword,
+    rpcHost,
+    rpcPort
+  } = config[crypto]
 
-const rpcRequest = async (body) => {
   const response = await fetch(`http://${rpcUser}:${rpcPassword}@${rpcHost}:${rpcPort}`, {
     method: "POST",
     headers: {
@@ -28,37 +29,54 @@ const rpcRequest = async (body) => {
   return json.result
 }
 
-app.get('/', async (req, res) => {
-  const getinfo = await rpcRequest({"jsonrpc": "1.0", "id":"curl", "method": "getinfo", "params": [] })
-  const getbalance = await rpcRequest({"jsonrpc": "1.0", "id":"curl", "method": "getbalance", "params": [] })
-  const getservers = await rpcRequest({"jsonrpc": "1.0", "id":"curl", "method": "getservers", "params": [] })
+const getCryptoShortened = (crypto) => {
+  const cryptos = {
+    "bitcoin": "BTC",
+    "litecoin": "LTC",
+    "bitcoin-cash": "BCH"
+  }
+  return cryptos[crypto]
+}
 
-  res.render("index", {
-    domain: req.headers.host,
-    ...getinfo,
-    ...getbalance,
-    servers: Object.keys(getservers).map(host => {
-      return {
-        host,
-        pruning: getservers[host].pruning ?? "-",
-        version: getservers[host].version ?? "-",
-        ssl: getservers[host].s ?? "-",
-        tcp: getservers[host].t ?? "-",
-      }
-    }),
-    crypto
-  });
-})
+[
+  "bitcoin",
+  "litecoin",
+  "bitcoin-cash"
+].forEach(crypto => {
+  app.get(`/${crypto}`, async (req, res) => {
+    const getinfo = await rpcRequest({"jsonrpc": "1.0", "id":"curl", "method": "getinfo", "params": [] }, crypto)
+    const getbalance = await rpcRequest({"jsonrpc": "1.0", "id":"curl", "method": "getbalance", "params": [] }, crypto)
+    const getservers = await rpcRequest({"jsonrpc": "1.0", "id":"curl", "method": "getservers", "params": [] }, crypto)
 
-app.get("/unspent", async (req, res) => {
-  const listunspent = await rpcRequest({"jsonrpc": "1.0", "id":"curl", "method": "listunspent", "params": [] })
+    res.render("index", {
+      domain: req.headers.host,
+      ...getinfo,
+      ...getbalance,
+      servers: Object.keys(getservers).map(host => {
+        return {
+          host,
+          pruning: getservers[host].pruning ?? "-",
+          version: getservers[host].version ?? "-",
+          ssl: getservers[host].s ?? "-",
+          tcp: getservers[host].t ?? "-",
+        }
+      }),
+      crypto: getCryptoShortened(crypto),
+      basePath: crypto
+    });
+  })
 
-  res.render("unspent", {
-    domain: req.headers.host,
-    total: listunspent.reduce((acc, { value }) => acc+parseFloat(value), 0),
-    listunspent,
-    crypto
-  });
+  app.get(`/${crypto}/unspent`, async (req, res) => {
+    const listunspent = await rpcRequest({"jsonrpc": "1.0", "id":"curl", "method": "listunspent", "params": [] }, crypto)
+
+    res.render("unspent", {
+      domain: req.headers.host,
+      total: listunspent.reduce((acc, { value }) => acc+parseFloat(value), 0),
+      listunspent,
+      crypto: getCryptoShortened(crypto),
+      basePath: crypto
+    });
+  })
 })
 
 const port = process.env.port || 3000
