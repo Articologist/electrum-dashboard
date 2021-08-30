616d24eb30c64970891b63acfa4194b0e1d17b92
Daniele Servadei <servadei.d@gmail.com>
2021-08-30 12:33:12 +0200

    add dark theme

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const fs = require("fs")
 const path = require("path")
 const app = express()
 
+app.use(express.static(path.join(__dirname, "public")))
 app.set("views", path.join(__dirname, "/pages"))
 app.set("view engine", "ejs")
 
